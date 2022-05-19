b80d9d02c769ac37ac7bcbd0f62ad37a755797e2
PabloThePeruna <eveliina.lehtinen@edu.turkuamk.fi>
2022-05-19 11:45:56 +0300

    More buttons and added BoilerLibrary as a Scene

--- Scanning application/Assets/Script/ChangeScene.cs
+++ Scanning application/Assets/Script/ChangeScene.cs
@@ -2,11 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ChangeScene : MonoBehaviour
 {
+    // Changes the scene
     public void ChangeToScene(string sceneToChangeTo)
     {
         SceneManager.LoadScene(sceneToChangeTo);
     }
+
+    // Disables the button given
+    public void DisableButton (GameObject buttonToDisable)
+    {
+        buttonToDisable.SetActive(false);
+    }
+
+    public void EnableButton (GameObject buttonToEnable)
+    {
+        buttonToEnable.SetActive(true);
+    }
+
+
 }
