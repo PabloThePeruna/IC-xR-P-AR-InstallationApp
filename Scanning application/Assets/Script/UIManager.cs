759b29e59b1d8f7e6f70d1280bcca95b4963b790
PabloThePeruna <eveliina.lehtinen@edu.turkuamk.fi>
2022-08-08 18:21:41 +0300

    Added a screenshotEffect when the button is pressed

--- Scanning application/Assets/Script/UIManager.cs
+++ Scanning application/Assets/Script/UIManager.cs
@@ -2,9 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+
+    public GameObject screenshotScreen;
+
+    void Update()
+    {
+        //if (screenshotScreen != false)
+        //{
+            //StartCoroutine(FadeOut());
+        //}
+    }
+
     // Changes the scene
     public void ChangeToScene(string sceneToChangeTo)
     {
@@ -21,5 +33,26 @@ public class UIManager : MonoBehaviour
     public void EnableButton(GameObject buttonToEnable)
     {
         buttonToEnable.SetActive(true);
+    }
+
+    public void ScreenshotEffect(GameObject shotButton)
+    {
+        screenshotScreen.SetActive(true);
+        StartCoroutine(FadeOut());
+    }
+
+
+    // fade from opaque to transparent
+    IEnumerator FadeOut()
+    {
+        // loop over 1 second backwards
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            // set color with i as alpha
+            screenshotScreen.GetComponent<Image>().color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+
+        screenshotScreen.SetActive(false);
     }
 }
