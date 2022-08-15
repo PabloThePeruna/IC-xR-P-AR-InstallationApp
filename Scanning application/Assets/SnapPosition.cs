dc029152bae23992a694794a49acaae11a3ebcb8
NoSkill4me <kesterfritz@gmail.com>
2022-08-15 16:36:16 +0200

    Trouble with NativeArray

--- Scanning application/Assets/SnapPosition.cs
+++ Scanning application/Assets/SnapPosition.cs
@@ -10,7 +10,7 @@ public class SnapPosition : MonoBehaviour
     //public BoundedPlane MyBoundedPlane;
     public ARPlane MyARPlane;
     //private SphereCollider CheckArea = new SphereColider;
-    private Vector3 [] BoundaryPoints = new Vector3 [100];//This here is only temporary until I can get the boundary points, to test for Errors in Unity
+    private Vector2 [] BoundaryPointsInPlaneSpace = new Vector2 [100];//This here is only temporary until I can get the boundary points, to test for Errors in Unity
     // Start is called before the first frame update
     void Start()
     {
@@ -22,12 +22,13 @@ public class SnapPosition : MonoBehaviour
     {
         Vector3 PlaneNormal = MyARPlane.normal;//This can probably be placed in the recalculation of the vector2
         Vector3 PlaneOrigin = MyARPlane.transform.position;
-        //BoundaryPoints = MyARPlane.boundary;
-            for (int i = 0; i < BoundaryPoints.Length; i++)
+        
+        BoundaryPointsInPlaneSpace = MyARPlane.boundary;
+            for (int i = 0; i < BoundaryPointsInPlaneSpace.Length; i++)
             {
-                if (Vector3.Distance(transform.position, BoundaryPoints[i]) < 0.01f)
+                if (Vector3.Distance(transform.position, BoundaryPointsInPlaneSpace[i]) < 0.01f)
                 {
-                    transform.position = BoundaryPoints[i];
+                    transform.position = BoundaryPointsInPlaneSpace[i];
                 }
             }
         
