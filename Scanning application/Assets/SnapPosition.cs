2885ab6d0a6e3447c3131b81a20fff4a33385f16
NoSkill4me <kesterfritz@gmail.com>
2022-08-15 17:18:14 +0200

    Maths suck
    Somehow I need to tranform the BoundaryPoints from 2d plane space to 3d world space
    This is really a problem, because we would need the rotational orientation of the plane space to the world space for the maths

--- Scanning application/Assets/SnapPosition.cs
+++ Scanning application/Assets/SnapPosition.cs
@@ -10,7 +10,8 @@ public class SnapPosition : MonoBehaviour
     //public BoundedPlane MyBoundedPlane;
     public ARPlane MyARPlane;
     //private SphereCollider CheckArea = new SphereColider;
-    private Vector2 [] BoundaryPointsInPlaneSpace = new Vector2 [100];//This here is only temporary until I can get the boundary points, to test for Errors in Unity
+    private Vector2 [] BoundaryPointsInPlaneSpace;//This will store the Boundary Points, it's 2d because its place space
+    private Vector3 [] BoundaryPointsInWorldSpace;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,11 +23,15 @@ public class SnapPosition : MonoBehaviour
     {
         Vector3 PlaneNormal = MyARPlane.normal;//This can probably be placed in the recalculation of the vector2
         Vector3 PlaneOrigin = MyARPlane.transform.position;
-        
+        Vector3 PlaneCenterWorldSpace = MyARPlane.center;
+        Vector2 PlaneCenterPlaneSpace = MyARPlane.centerInPlaneSpace;
         BoundaryPointsInPlaneSpace = MyARPlane.boundary.ToArray();
-            for (int i = 0; i < BoundaryPointsInPlaneSpace.Length; i++)
+
+        //Somehow I need to tranform the BoundaryPoints from 2d plane space to 3d world space
+        //This is really a problem, because I would need the rotational orientation of the plane space to the world space for the maths
+            for (int i = 0; i < BoundaryPointsInWorldSpace.Length; i++)
             {
-                if (Vector3.Distance(transform.position, BoundaryPointsInPlaneSpace[i]) < 0.01f)
+                if (Vector3.Distance(transform.position, BoundaryPointsInWorldSpace[i]) < 0.01f)
                 {
                     transform.position = BoundaryPointsInPlaneSpace[i];
                 }
