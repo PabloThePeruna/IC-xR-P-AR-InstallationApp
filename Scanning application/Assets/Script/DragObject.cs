cabb5ddfbcfb204618d7d5d4b657a567ef4b4777
NoSkill4me <kesterfritz@gmail.com>
2022-08-24 14:17:19 +0200

    Experimenting with Dragginga and Rotation

--- Scanning application/Assets/Script/DragObject.cs
+++ Scanning application/Assets/Script/DragObject.cs
@@ -9,61 +9,35 @@ using UnityEngine;
 public class DragObject : MonoBehaviour
 
 {
-
+    //Translate Position Variables
     private Vector3 mOffset;
-
-
-
     private float mZCoord;
+
+    //Rotate Position Variables
 
 
 
     void OnMouseDown()
-
     {
-
         mZCoord = Camera.main.WorldToScreenPoint(
-
             gameObject.transform.position).z;
-
-
-
         // Store offset = gameobject world pos - mouse world pos
-
         mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
-
     }
-
-
-
     private Vector3 GetMouseAsWorldPoint()
-
     {
-
         // Pixel coordinates of mouse (x,y)
-
         Vector3 mousePoint = Input.mousePosition;
-
-
-
         // z coordinate of game object on screen
-
         mousePoint.z = mZCoord;
-
-
-
         // Convert it to world points
-
         return Camera.main.ScreenToWorldPoint(mousePoint);
-
     }
 
 
 
     void OnMouseDrag()
-
     {
-
         transform.position = GetMouseAsWorldPoint() + mOffset;
 
     }
