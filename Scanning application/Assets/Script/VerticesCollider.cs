6a2079ee898421628b956fda688b7404d14a3c8c
NoSkill4me <kesterfritz@gmail.com>
2022-08-31 15:01:58 +0200

    Coroutine Collision Detection [Untested]
    Since InvokeRepeating caused Coloring issues witht he Mesh, hopefully using the Collision as a Coroutine will solve the Performance Problems of Large Meshes

--- Scanning application/Assets/Script/VerticesCollider.cs
+++ Scanning application/Assets/Script/VerticesCollider.cs
@@ -18,48 +18,112 @@ public class VerticesCollider : MonoBehaviour
     [Tooltip("The colour that the part of the mesh without collision takes during collision")]
     public Color NonColliding = new Color(0, 0, 0, 0);
 
+    [Tooltip("This will be used to switch between the inefficient Collision Test and the inefficient Collision Test that only happens every WaitNFrames as a Coroutine")]
+    public bool InvokeRepeatingTest = false;
+
+    [Tooltip("This is the number of Frames the Program waits between Collision Checks")]
+    public int WaitNFrames = 20;
     private static bool colliding = false;
+    private static int NumberOfWaitFrames = 0;
+    private static Color[] ColorStorage;
+
     void Start()
     {
         //Using the following function, we only Scan the Mesh every 0.1 seconds for collision, for Performance reasons
         //This may not be strictly necessary but it definitely can't hurt. - As it turns out, this causes flickering of the mesh color, where the mesh turn to it's base color.
         //It would be very good for performance if we could use this.
-        //InvokeRepeating("ScanMeshForCollision", 0.5f, 0.1f);//While this may be a nice idea to improve performance, it causes the mesh to turn white and flashing. Unless this can be eliminted, use update()
+        //InvokeRepeating("ScanMeshForCollision", 0.5f, 0.1f);//While this may be a nice idea to improve performance, it causes the mesh to turn white and flashing. Unless this is fixed, use update()
     }
 
     void Update()
     {
+        //The idea is, we choose between the inefficient Collision Test and the inefficient Collision Test that only happens every WaitNFrames as a coroutine
+        //Should therefore hopefully keep things fluid
+        if (InvokeRepeatingTest)
+        {          
+            if (NumberOfWaitFrames == WaitNFrames)                          //If we are in a TestFrame
+            {
+                NumberOfWaitFrames = 0;                                     //Reset the Test Frames
+                StartCoroutine(ScanMeshForCollision());                     //Have the Collision Check as a Coroutine
+            }
+            else
+            {
+                NumberOfWaitFrames++;                                       //Increase the Number of Frames
+                Mesh mesh = GetComponent<MeshFilter>().mesh;                //Get the mesh
+                for (int i = 0; i < ColorStorage.Length; i++)               //Since I am not sure that the number of vertices stayed the same, only use as many colors as we have
+                {
+                    mesh.colors[i] = ColorStorage[i];                       //This could cause major Problems if it turns out the Mesh Generation changes the 
+                }
+            }
+        }
+        else
+        {
+            Mesh mesh = GetComponent<MeshFilter>().mesh;
+            Vector3[] vertices = mesh.vertices;
+            //Vector3[] normals = mesh.normals;
+            Color[] colors = new Color[vertices.Length];
 
+            for (var i = 0; i < vertices.Length; i++)
+            {
+                if (Physics.OverlapSphere(transform.position + vertices[i], 0f).Length != 0)
+                {
+                    //Debug.Log("colliding");
+                    colors[i] = Notice;
+                    colliding = true;
+
+                }
+                else if (colliding == true)
+                {
+                    //Debug.Log("No collision here");
+                    colors[i] = NonColliding;//This makes the mesh invisble where there is no collision, if there is collision elsewhere
+                }
+                else
+                {
+                    //Debug.Log("Miss");
+                    colors[i] = Standard;//This is if there is no collision anywhere
+                }
+
+                mesh.colors = colors;//is it maybe faster if we instantly write to mesh.colors instead of colors first?
+            }
+
+
+            //Debug.LogWarning("Collision check ended");
+        }
+
+    }
+
+    IEnumerator ScanMeshForCollision()
+    {
+        
         Mesh mesh = GetComponent<MeshFilter>().mesh;
         Vector3[] vertices = mesh.vertices;
         //Vector3[] normals = mesh.normals;
-        Color[] colors = new Color[vertices.Length];
+        ColorStorage = new Color[vertices.Length];
 
         for (var i = 0; i < vertices.Length; i++)
         {
             if (Physics.OverlapSphere(transform.position + vertices[i], 0f).Length != 0)
             {
                 //Debug.Log("colliding");
-                colors[i] = Notice;
+                ColorStorage[i] = Notice;
                 colliding = true;
-    
+
             }
-            else if(colliding==true)
+            else if (colliding == true)
             {
                 //Debug.Log("No collision here");
-                colors[i] = NonColliding;//This makes the mesh invisble where there is no collision, if there is collision elsewhere
+                ColorStorage[i] = NonColliding;//This makes the mesh invisble where there is no collision, if there is collision elsewhere
             }
             else
             {
                 //Debug.Log("Miss");
-                colors[i] = Standard;//This is if there is no collision anywhere
+                ColorStorage[i] = Standard;//This is if there is no collision anywhere
             }
 
-            mesh.colors = colors;//is it maybe faster if we instantly write to mesh.colors instead of colors first?
+            mesh.colors = ColorStorage;//is it maybe faster if we instantly write to mesh.colors instead of colors first?
         }
-
-
-        //Debug.LogWarning("Collision check ended");
+        
+        yield return null;
     }
 }
     //This is the Code Johannes wrote for us, it is probably more efficient/better than what we have but I don't understand it.
